cbe82639b328cb8231e68894a8468fbb7dc26fc7
Mohammed <khamismitch@gmail.com>
2024-01-29 09:52:08 +0300

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,16 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start=1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(fruits):
+    return [fruit.capitalize() + '!' for fruit in fruits]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(cheeses):
+    cheese_types = ["cheddar", "gouda", "brie"]  
+    for cheese in cheeses:
+        if cheese in cheese_types:
+            return cheese
+    return None
